296edc9dd9be0055efa6327c2ff2b684e93451ff
arnab072000 <arnab11kr@gmail.com>
2024-08-12 14:16:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var inputItem = document.getElementById('add-todo');
+var listContainer = document.getElementById('list-container');
+//type Todo = string;
+// interface Todo {
+//     todo: string[]
+// }
+var todos = JSON.parse(localStorage.getItem('todos') || '[]');
+loadTodos();
+function addTodo() {
+    var todoValue = inputItem.value.trim();
+    console.log(todoValue);
+    if (todoValue === '' || !isNaN(Number(todoValue))) {
+        alert('Write some text ...!');
+    }
+    else {
+        todos.push(todoValue);
+        createList(todoValue);
+        inputItem.value = '';
+        saveTodo();
+    }
+}
+function createList(value) {
+    var li = document.createElement('li');
+    li.innerHTML = "\n        <input type=\"checkbox\">\n        <label>".concat(value, "</label>\n    ");
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteTodo';
+    deleteButton.onclick = function () {
+        todos = todos.filter(function (todo) { return todo !== value; });
+        listContainer.removeChild(li);
+        saveTodo();
+    };
+    li.appendChild(deleteButton);
+    listContainer.appendChild(li);
+}
+function saveTodo() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+function loadTodos() {
+    todos.forEach(createList);
+}
+function clearTodo() {
+    listContainer.innerHTML = '';
+    todos = [];
+    saveTodo();
+}
---
+++ index.ts
@@ -0,0 +1,63 @@
+const inputItem = document.getElementById('add-todo') as HTMLInputElement;
+const listContainer = document.getElementById('list-container') as HTMLUListElement;
+
+//type Todo = string;
+// interface Todo {
+//     todo: string[]
+// }
+
+let todos: string[] = JSON.parse(localStorage.getItem('todos') || '[]');
+
+loadTodos();
+
+function addTodo(): void {
+    const todoValue = inputItem.value.trim();
+    console.log(todoValue);
+    
+    if (todoValue === '' || !isNaN(Number(todoValue))) {
+        alert('Write some text ...!');
+    } else {
+        todos.push(todoValue);
+        createList(todoValue);
+        inputItem.value = '';
+        saveTodo();
+    }
+}
+
+function createList  (value: string): void {
+    const li = document.createElement('li');
+    
+    li.innerHTML = `
+        <input type="checkbox">
+        <label>${value}</label>
+    `;
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteTodo';
+    
+    deleteButton.onclick = function (): void {
+        todos = todos.filter(todo => todo !== value);
+        listContainer.removeChild(li);
+        saveTodo();
+    };
+    
+    li.appendChild(deleteButton);
+    listContainer.appendChild(li);
+}
+
+function saveTodo(): void {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function loadTodos(): void {
+    todos.forEach(createList);
+}
+
+function clearTodo(): void {
+    listContainer.innerHTML = '';
+    todos = [];
+    saveTodo();
+}
+
+
